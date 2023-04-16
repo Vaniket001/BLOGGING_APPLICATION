55a6c9aab101129bba3ba19f5f0a85cb2fb8597f
Aniket <kumaraniket900@gmail.com>
2023-04-16 09:36:52 +0530

    First Commit For Blogging Application

---
+++ src/main/java/com/lcwd/blog/BloggingApplication.java
@@ -0,0 +1,13 @@
+package com.lcwd.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApplication.class, args);
+	}
+
+}

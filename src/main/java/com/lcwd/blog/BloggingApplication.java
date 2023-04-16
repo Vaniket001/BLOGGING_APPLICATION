79c65b86372f188a96b3467aba663cfe45ff6bcb
Aniket <kumaraniket900@gmail.com>
2023-04-16 09:56:43 +0530

    changes

--- src/main/java/com/lcwd/blog/BloggingApplication.java
+++ src/main/java/com/lcwd/blog/BloggingApplication.java
@@ -8,7 +8,7 @@ public class BloggingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingApplication.class, args);
-		System.out.println("BloggingApplication stared !!");
+		System.out.println("BloggingApplication started !!");
 	}
 
 }

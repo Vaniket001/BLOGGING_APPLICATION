8f3a952f5abf9aec9de1198c0a18f64b67fc38ee
Aniket <kumaraniket900@gmail.com>
2023-05-05 11:43:31 +0530

    controller method and Global exception is added

--- src/main/java/com/lcwd/blog/controller/UserController.java
+++ src/main/java/com/lcwd/blog/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.lcwd.blog.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lcwd.blog.payload.ApiResponse;
 import com.lcwd.blog.payload.UserDto;
 import com.lcwd.blog.service.UserService;
 
@@ -23,5 +30,25 @@ public class UserController {
 		UserDto createdUserDto=this.userService.createUser(userDto);
 		return new ResponseEntity<UserDto>(createdUserDto, HttpStatus.CREATED);
 	}
-
+	
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+		return new ResponseEntity<UserDto>(userService.updateUser(userDto, uid),HttpStatus.OK);
+	}
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable("userId") Integer uid){
+		return new ResponseEntity<UserDto>(userService.getUserById(uid),HttpStatus.OK);
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<List<UserDto>> getAllUsers(){
+		return new ResponseEntity<List<UserDto>>(userService.getAllUsers(),HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uid){
+		userService.deleteUser(uid);
+		return new ResponseEntity<ApiResponse>(new ApiResponse("User with userId : "+ uid + " is deleted successfully !!",true),HttpStatus.OK);
+	}
 }

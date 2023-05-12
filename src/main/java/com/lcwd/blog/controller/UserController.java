3f862b0d4ac5b17998a32d201a3cce3791fc20cf
Aniket <kumaraniket900@gmail.com>
2023-05-12 16:30:32 +0530

    Validation DOne !!

--- src/main/java/com/lcwd/blog/controller/UserController.java
+++ src/main/java/com/lcwd/blog/controller/UserController.java
@@ -2,6 +2,8 @@ package com.lcwd.blog.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,13 +28,13 @@ public class UserController {
 	private UserService userService;
 	
 	@PostMapping("/")
-	public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto){
 		UserDto createdUserDto=this.userService.createUser(userDto);
 		return new ResponseEntity<UserDto>(createdUserDto, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/{userId}")
-	public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
+	public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid){
 		return new ResponseEntity<UserDto>(userService.updateUser(userDto, uid),HttpStatus.OK);
 	}
 	
